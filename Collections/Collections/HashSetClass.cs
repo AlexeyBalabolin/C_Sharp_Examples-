c0f49f27d07f8f6c32cbd6420547e2dfeccdfdd4
Alexey <balabolinad@gmail.com>
2022-02-09 10:18:11 +0300

    rar deleted

---
+++ Collections/Collections/HashSetClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework8
+{
+    class HashSetClass
+    {
+        public HashSet<int> FillHashSet(HashSet<int> currentHashSet)
+        {
+            while(true)
+            {
+                Console.WriteLine("Введите число, которое нужно добавить");
+                int number = int.Parse(Console.ReadLine());
+                if (currentHashSet.Add(number))
+                {
+                    Console.WriteLine("Число добавлено");
+                }
+                else
+                {
+                    Console.WriteLine("Число уже есть в коллекции");
+                    break;
+                }
+            }
+            return currentHashSet;
+        }
+
+        public void Print(HashSet<int> currentHashSet)
+        {
+            foreach (var v in currentHashSet)
+                Console.Write($"{v} ");
+        }
+    }
+}
